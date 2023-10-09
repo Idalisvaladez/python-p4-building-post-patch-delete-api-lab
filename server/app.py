9f6381b99c25239336e773db89ab09d3d2993091
Idalis Valadez <idalisvaladez22@gmail.com>
2023-10-09 18:03:22 -0500

    Complete lab

--- server/app.py
+++ server/app.py
@@ -30,18 +30,60 @@ def bakeries():
     )
     return response
 
-@app.route('/bakeries/<int:id>')
+@app.route('/baked_goods', methods = ['GET', 'POST'])
+def baked_goods():
+    if request.method == 'GET':
+        baked_goods = [goods.to_dict() for goods in BakedGood.query.all()]
+
+        return make_response(baked_goods, 200)
+    
+    elif request.method == 'POST':
+        new_good = BakedGood(
+            name = request.form.get('name'),
+            price = request.form.get('price'), 
+            bakery_id = request.form.get('baker_id')
+        )
+
+        db.session.add(new_good)
+        db.session.commit()
+
+        return make_response(new_good.to_dict(), 201)
+    
+@app.route('/baked_goods/<int:id>', methods = ['GET', 'DELETE'])
+def baked_goods_by_id(id):
+    baked_good = BakedGood.query.filter_by(id = id).first()
+
+    if request.method == 'GET':
+        return make_response(baked_good.to_dict(), 200)
+    
+    elif request.method == 'DELETE':
+        db.session.delete(baked_good)
+        db.session.commit()
+
+        response_body = {
+            "delete successful": True,
+            "message": "Baked good deleted"
+        }
+
+        return make_response(response_body, 200)
+    
+@app.route('/bakeries/<int:id>', methods = ['GET', 'PATCH'])
 def bakery_by_id(id):
 
     bakery = Bakery.query.filter_by(id=id).first()
-    bakery_serialized = bakery.to_dict()
 
-    response = make_response(
-        bakery_serialized,
-        200
-    )
-    return response
+    if request.method == 'GET':
+        return make_response(bakery.to_dict(), 200)
+    
+    elif request.method == 'PATCH':
+        for attr in request.form:
+            setattr(bakery, attr, request.form.get(attr))
 
+        db.session.add(bakery)
+        db.session.commit()
+
+        return make_response(bakery.to_dict(), 200)
+    
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
     baked_goods_by_price = BakedGood.query.order_by(BakedGood.price).all()
